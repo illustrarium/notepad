dc32c0ef9f225fd2a897cb121620d1b8b25c3b20
Aleksey Paschenko <paschenko.aleks@gmail.com>
2017-02-25 19:55:07 +0300

    post class + ancestors

---
+++ memo.rb
@@ -0,0 +1,11 @@
+class Memo < Post
+
+    def read_fsrom_console
+      #todo
+    end
+
+    def to_string
+      #todo
+    end
+
+end     
---
+++ task.rb
@@ -0,0 +1,15 @@
+class Task < Post
+
+    def initialize
+        super
+    end
+
+    def read_fsrom_console
+      #todo
+    end
+
+    def to_string
+      #todo
+    end
+
+end
